bdab6cb4b310cdd602fdce0324c2ef92e11ab6f9
shivaprasad-bhat <shivaprasad@svbneelmane.me>
2022-01-15 01:04:56 +0530

    feat(Split-Trip): Header, Content with Round Button created

--- app/src/main/java/me/svbneelmne/compose/splittrip/MainActivity.kt
+++ app/src/main/java/me/svbneelmne/compose/splittrip/MainActivity.kt
@@ -1,25 +1,37 @@
 package me.svbneelmne.compose.splittrip
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.text.isDigitsOnly
 import me.svbneelmne.compose.splittrip.MainActivity.Companion.INR_Symbol
+import me.svbneelmne.compose.splittrip.components.InputField
 import me.svbneelmne.compose.splittrip.ui.theme.SplitTripTheme
+import me.svbneelmne.compose.splittrip.widgets.RoundIconButton
 
 class MainActivity : ComponentActivity() {
 
@@ -27,11 +39,15 @@ class MainActivity : ComponentActivity() {
         const val INR_Symbol = "\u20B9"
     }
 
+    @ExperimentalComposeUiApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApp {
-                Header()
+                Column {
+                    Header()
+                    MainContent()
+                }
             }
         }
     }
@@ -76,7 +92,86 @@ fun Header(totalPerPerson: Double = 134.4) {
     }
 }
 
+@ExperimentalComposeUiApi
+@Preview
+@Composable
+fun MainContent() {
+    BillForm() { billAmount ->
+        Log.d("SPLITTRIP", "MainContent: $billAmount")
+    }
+}
 
+@ExperimentalComposeUiApi
+@Composable
+fun BillForm(
+    modifier: Modifier = Modifier,
+    onValChange: (String) -> Unit = {}
+) {
+    val totalBillState = remember {
+        mutableStateOf("")
+    }
+    val validState = remember(totalBillState.value) {
+        totalBillState.value.trim().isNotEmpty() && totalBillState.value.trim().isDigitsOnly()
+    }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(
+        modifier = modifier
+            .padding(2.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(corner = CornerSize(8.dp)),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray)
+    ) {
+        Column(
+            modifier = modifier.padding(8.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.Start
+        ) {
+            InputField(
+                valueState = totalBillState,
+                labelId = "Enter the bill amount",
+                enabled = true,
+                onAction = KeyboardActions {
+                    if (!validState) return@KeyboardActions
+                    onValChange(totalBillState.value.trim())
+                    keyboardController?.hide()
+                }
+            )
+            if (validState) {
+                Row(
+                    modifier = modifier.padding(3.dp),
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    Text(
+                        text = "Split",
+                        modifier = modifier.align(alignment = Alignment.CenterVertically)
+                    )
+                    Spacer(modifier = modifier.width(120.dp))
+                    Row(
+                        modifier = modifier.padding(horizontal = 3.dp),
+                        horizontalArrangement = Arrangement.End
+                    ) {
+                        RoundIconButton(
+                            imageVector = Icons.Default.Remove,
+                            onClick = {
+                                Log.d("IconClick", "Minus Clicked")
+                            }
+                        )
+                        RoundIconButton(
+                            imageVector = Icons.Default.Add,
+                            onClick = {
+                                Log.d("IconClick", "Plus Clicked")
+
+                            }
+                        )
+                    }
+                }
+            } else {
+                Box {}
+            }
+        }
+
+    }
+}
 
 //@Preview(showBackground = true)
 @Composable
