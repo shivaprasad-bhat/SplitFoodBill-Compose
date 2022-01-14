d55333588811ed871f10f16ddde93f1a3af403f0
shivaprasad-bhat <shivaprasad@svbneelmane.me>
2022-01-14 23:27:59 +0530

    feat(Split-Trip): Added Component - Input Field

---
+++ app/src/main/java/me/svbneelmne/compose/splittrip/components/Components.kt
@@ -0,0 +1,53 @@
+package me.svbneelmne.compose.splittrip.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import me.svbneelmne.compose.splittrip.MainActivity
+
+@Composable
+fun InputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    labelId: String,
+    enabled: Boolean,
+    isSingleLine: Boolean = true,
+    keyboardType: KeyboardType = KeyboardType.Number,
+    imeAction: ImeAction = ImeAction.Next,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+
+    OutlinedTextField(
+        value = valueState.value,
+        onValueChange = { valueState.value = it },
+        modifier = modifier.padding(bottom = 10.dp, start = 10.dp, end = 10.dp),
+        label = { Text(text = labelId) },
+        leadingIcon = {
+//            Icon(imageVector = Icons.Rounded.AttachMoney, contentDescription = "Money Icon")
+            Text(text = MainActivity.INR_Symbol)
+        },
+        singleLine = isSingleLine,
+        textStyle = TextStyle(
+            fontSize = 18.sp,
+            color = MaterialTheme.colors.onBackground
+        ),
+        enabled = enabled,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = keyboardType,
+            imeAction = imeAction
+        ),
+        keyboardActions = onAction
+    )
+
+}
