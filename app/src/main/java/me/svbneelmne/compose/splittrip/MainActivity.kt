9e17864c3a65834c4d8cbcea6b55fa0c88ca5236
shivaprasad-bhat <shivaprasad@svbneelmane.me>
2022-01-14 22:39:47 +0530

    feat(Split-Trip): Added Top Header

--- app/src/main/java/me/svbneelmne/compose/splittrip/MainActivity.kt
+++ app/src/main/java/me/svbneelmne/compose/splittrip/MainActivity.kt
@@ -3,36 +3,85 @@ package me.svbneelmne.compose.splittrip
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import me.svbneelmne.compose.splittrip.MainActivity.Companion.INR_Symbol
 import me.svbneelmne.compose.splittrip.ui.theme.SplitTripTheme
 
 class MainActivity : ComponentActivity() {
+
+    companion object {
+        const val INR_Symbol = "\u20B9"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SplitTripTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                Header()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    SplitTripTheme {
+        Surface(color = MaterialTheme.colors.background) {
+            content()
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Header(totalPerPerson: Double = 134.4) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(150.dp)
+            .clip(shape = RoundedCornerShape(corner = CornerSize(12.dp))),
+        //  .clip(shape = CircleShape.copy(app = CornerSize(12.dp)) can be used also
+        color = Color(0xFF8D9BEE)
+    ) {
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val total = "%.2f".format(totalPerPerson)
+            Text(
+                text = "Total per Person",
+                style = MaterialTheme.typography.h5
+            )
+            Text(
+                text = "$INR_Symbol $total",
+                style = MaterialTheme.typography.h4,
+                fontWeight = FontWeight.ExtraBold
+            )
+        }
+    }
+}
+
+
+
+//@Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    SplitTripTheme {
-        Greeting("Android")
+    MyApp {
+        Text(text = "Hello Again")
     }
 }
